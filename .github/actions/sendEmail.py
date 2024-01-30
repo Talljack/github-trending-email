d118b4ea0e9f0443757fec124fffcdac336126de
Yugang Cao <1442770751@qq.com>
2024-01-30 20:39:37 +0800

    fix: test (#6)
    * fix: test
    
    * fix: test
    
    * fix: test

--- .github/actions/sendEmail.py
+++ .github/actions/sendEmail.py
@@ -1,8 +1,19 @@
 import yagmail
 import sys
-# gpus = sys.argv[1]
-# #gpus = [int(gpus.split(','))]
-# batch_size = sys.argv[2]
-# print gpus
-# print batch_size
+username = sys.argv[1]
+password = sys.argv[2]
+repo_data = sys.argv[3]
 
+def send_email():
+    try:
+        content = ''
+        for key, repos in repo_data:
+            for index, repo in repos:
+                content += "{title}--{description}-{language}-{stars}-{todayStars}".format(repo.title, repo.description, repo.language, repo.stars, repo.todayStars)
+        yag = yagmail.SMTP('${{ secrets.GMAIL_USERNAME }}', '${{ secrets.GMAIL_PASSWORD }}')
+        yag.send(to='${{ secrets.GMAIL_USERNAME }}', subject='Github trending repos', contents=content)
+        print('Email sent successfully')
+    except Exception as e:
+        print(f'Failed to send email: {e}')
+
+send_email()
