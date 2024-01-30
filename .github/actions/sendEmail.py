0bda7ae9a5fb73c1ab0f00d299c5ebc91213ec85
Yugang Cao <1442770751@qq.com>
2024-01-30 20:55:13 +0800

    feat: test (#8)
    * feat: test
    
    * feat: test

--- .github/actions/sendEmail.py
+++ .github/actions/sendEmail.py
@@ -1,7 +1,9 @@
 # send_email.py
 import yagmail
+import json
 
 def send_email(username, password, recipient, subject, body):
+    print('username', username, 'subject', subject)
     yag = yagmail.SMTP(username, password)
     yag.send(to=recipient, subject=subject, contents=body)
     print('Email sent successfully')
@@ -10,7 +12,10 @@ if __name__ == '__main__':
     import sys
     content = ''
     repo_data = sys.argv[5]
+    repo_data = json.loads(repo_data)
     for key, repos in repo_data:
         for index, repo in repos:
             content += "{title}--{description}-{language}-{stars}-{todayStars}".format(repo.title, repo.description, repo.language, repo.stars, repo.todayStars)
-    send_email(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+
+    print('content', content)
+    send_email(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], content)
