e15cadbbbeb0a4965e6e2d5aadb9b50ac60a3b49
Yugang Cao <yugang.cao12@gmail.com>
2024-04-21 14:20:19 +0800

    fix: update

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,12 @@ import createEslintConfig from 'talljack-eslint-config'
 export default createEslintConfig({
   typescript: true,
   formatters: true,
-  jsdoc: false,
   rules: {
     '@typescript-eslint/ban-ts-comment': 'off',
     'no-console': 'off',
   },
+}, {
+  ignores: [
+    '**/lib',
+  ],
 })
